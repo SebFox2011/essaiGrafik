e9ad965c6a36b284cd2084dcec5e5555d312256a
Seb <sebastien.philippe5@gmail.com>
2020-06-28 14:28:57 +0200

    Ajout du composant Essai

--- src/LogginForm.jsx
+++ src/LogginForm.jsx
@@ -4,33 +4,82 @@ import { Button } from './ui/Button'
 function Boiling({ celsius }, children) {
     console.log('render Boiling')
     //const [isBoiling, setBoiling] = useState(null)
-    if (celsius>90) {
+    if (celsius > 90) {
         return <div className='alert alert-danger mt-4'>'ca boue' </div>
     }
-    else{
+    else {
         return <div className='alert alert-info mt-4'>'ca boue pas' </div>
     }
-
-
-    
 }
 
-export class Essai extends React.Component  {
-    constructor(props){
-        super(props)
-        this.state = {
-            essai: 1,
-            //fonction: console.log('essai')
-        }
+export function Essai() {
+
+    const tabs = [{id:1,nom:'Sébastien',prenom:'Philippe'}, 
+    {id:2, nom: 'Claire', prenom:'Martin'}, 
+    {id:3,nom:'Francois' , prenom: 'Lefort'}, 
+    {id:4,nom:'gerard' , prenom: 'gris'}, 
+    {id:5,nom:'antoinette' , prenom: 'morein'}]
+
+    const [name,setName] = useState({
+        id:0,
+        nom:'nom',
+        prenom:'prenom'
+    })
+
+    function litNomPrenom (id)  {
+        let tab= []
+        tabs.forEach(element => {         
+            if (element.id === parseInt(id)){
+                tab.push(element.nom)
+                tab.push(element.prenom)
+                return tab
+            }
+        })
+        return tab
     }
 
-    render () {
-        const {fonction} = this.state
-        return <div>
-            {fonction}
-            </div>
-        
+    const handleChange = event => {
+        event.persist()
+        const [nom,prenom]= litNomPrenom(event.target.value)
+        setName(state => {return {...state,id:event.target.value,nom:nom,prenom:prenom} })
     }
+
+    const handleChangeName = event => {
+        event.persist()
+        const name = event.target.value
+        setName(state => {return {...state,nom:''} })
+        setName(state => {return {...state,nom:name}})
+    }
+
+    return <div className="container">
+        <div className="row align-items-end">
+            <div className="col-sm">
+                <div className="input-group mb-3">
+                    <div className="input-group-prepend">
+                        <label className="input-group-text" htmfor="inputGroupSelect01">Options</label>
+                    </div>
+                    <select className="custom-select" id="inputGroupSelect01" onChange={handleChange}>
+                        <option>Choose ...</option>
+                        {tabs.map((tab, index) => <option key={index} value={tab.id}>{tab.id}</option>)}
+                    </select>
+                </div>
+            </div>
+            <div className="col-sm">
+                <div className='form-group'>
+                    <label htmlFor='nom'>Nom</label>
+                    <input type='text' name='username' className='form-control' onChange={handleChangeName} value={name.nom} />
+                </div>
+            </div>
+            <div className="col-sm">
+                <div className='form-group'>
+                    <label htmlFor='prenom'>Prenom</label>
+                    <input type='text' name='username' className='form-control' readOnly value={name.prenom}/>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
 }
 
 export function Calculator() {
@@ -39,7 +88,7 @@ export function Calculator() {
     const handleOnChange = (e) => {
         e.preventDefault()
         setTemperature(e.target.value)
-        
+
     }
 
     return <div className='form-goup container'>
@@ -47,7 +96,7 @@ export function Calculator() {
         <input type="text" name="celsius" id="celsius" className='form-control'
             value={temperature} onChange={handleOnChange}
         />
-        <Boiling celsius={parseFloat( temperature)} />
+        <Boiling celsius={parseFloat(temperature)} />
     </div>
 }
 export function LogginForm() {
@@ -87,7 +136,5 @@ export function LogginForm() {
                 className='form-control' />
         </div>
         <Button type='submit' loading={isLoading}>Se connecter</Button>
-
     </form>
-
 }
